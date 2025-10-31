8b1b65adbc09ab82283c0e9da0bd8621295a936b
Chaeng2004 <mysnchyng23@gmail.com>
2025-10-31 23:09:16 +0800

    fix to import terminsl fees

--- IBT/src/App.jsx
+++ IBT/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Terminalfees from './pages/Terminalfees.jsx'
 
 function App() {
-  return
+  return (
+ < Terminalfees  />
+  )
 }
 
 export default App
