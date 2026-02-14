76aa53e177d4c0a0576eb10bcff993b5320dc9ab
Lol <133163245+Tantalost@users.noreply.github.com>
2026-02-14 15:49:56 +0800

    fix: nodemailer

--- IBT/backend/utils/sendEmail.js
+++ IBT/backend/utils/sendEmail.js
@@ -2,10 +2,15 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'smtp.gmail.com',
+    port: 587,              
+    secure: false,          
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      pass: process.env.EMAIL_PASS 
+    },
+    tls: {
+      rejectUnauthorized: false 
     }
   });
 
