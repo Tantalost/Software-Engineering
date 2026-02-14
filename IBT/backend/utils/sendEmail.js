cd4f765e928bd509d3c5f4a975be656c84762543
Lol <133163245+Tantalost@users.noreply.github.com>
2026-02-14 16:36:00 +0800

    brevo

--- IBT/backend/utils/sendEmail.js
+++ IBT/backend/utils/sendEmail.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: 'smtp-relay.brevo.com',
     port: 587,              
     secure: false,          
     auth: {
