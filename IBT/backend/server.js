05446e6eeb7923b8ff1098ea7b7b820fd4807aa9
Lol <133163245+Tantalost@users.noreply.github.com>
2026-02-23 15:23:02 +0800

    adding patch

--- IBT/backend/server.js
+++ IBT/backend/server.js
@@ -45,7 +45,7 @@ mongoose.connection.once('open', () => {
 app.use(cors({
     origin: "*", 
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
 }));
 
 app.use(express.json({ limit: '50mb' })); 
