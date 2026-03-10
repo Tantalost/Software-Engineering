e8c83458cc9ca112430f41189d92889ff2c0442f
Lol <133163245+Tantalost@users.noreply.github.com>
2026-03-10 16:13:55 +0800

    whiteskin

--- IBT/frontend/src/pages/AdminLogIn.jsx
+++ IBT/frontend/src/pages/AdminLogIn.jsx
@@ -250,7 +250,7 @@ export default function AdminLogin() {
                     type="button" 
                     onClick={handleForgotPasswordTrigger} 
                     disabled={isLoading} 
-                    className="text-sm font-semibold text-white-500 hover:text-white-400 transition-colors cursor-pointer"
+                    className="text-sm font-semibold text-white hover:text-white transition-colors cursor-pointer"
                   >
                     Forgot Password?
                   </button>
