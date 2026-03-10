8fe1de520f8ee33408761ccb47f362bb5803451b
Lol <133163245+Tantalost@users.noreply.github.com>
2026-03-11 05:10:07 +0800

    Mobile QR Landing Page

--- IBT/frontend/src/pages/LandingPage.jsx
+++ IBT/frontend/src/pages/LandingPage.jsx
@@ -168,7 +168,7 @@ const LandingPage = () => {
             
             <div className="bg-gray-50 p-4 rounded-xl border-2 border-dashed border-gray-300 w-64 h-64 flex items-center justify-center mb-6 shadow-inner">
               <img 
-                src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://your-app-link.com" 
+                src="MobileQR.png" 
                 alt="App QR Code" 
                 className="w-full h-full object-contain"
               />
