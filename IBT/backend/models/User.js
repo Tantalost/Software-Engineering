8f9dc700267d4b096d69c3922acc2cefea351a60
Chaeng2004 <mysnchyng23@gmail.com>
2026-03-26 16:46:09 +0800

    fix pfp

--- IBT/backend/models/User.js
+++ IBT/backend/models/User.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   mpin: { type: String },
-  
+  password: { type: String }, 
   
   firstName: { type: String },
   middleName: { type: String },
   lastName: { type: String },
   suffix: { type: String },
   contactNo: { type: String },
+  
+ 
+  avatarUrl: { type: String, default: null },
   
   otp: { type: String, default: null },
   otpExpires: { type: Date, default: null },
