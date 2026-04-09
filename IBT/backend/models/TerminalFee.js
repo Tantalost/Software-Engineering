74bf47e52e1a135513cc2319b8c9ce92348fcf70
Chaeng2004 <mysnchyng23@gmail.com>
2026-04-09 14:11:23 +0800

    adjusted the backend for ticket count

--- IBT/backend/models/TerminalFee.js
+++ IBT/backend/models/TerminalFee.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import Counter from "./Counter.js";
 
 const terminalFeeSchema = new mongoose.Schema({
   ticketNo: {
@@ -38,42 +37,5 @@ const terminalFeeSchema = new mongoose.Schema({
     default: false 
   }
 }, { timestamps: true });
-
-terminalFeeSchema.pre("save", async function (next) {
-  if (this.isNew) {
-    try {
-      // 1. Try to increment the counter
-      let counter = await Counter.findOneAndUpdate(
-        { id: "ticketNo" },
-        { $inc: { seq: 1 } },
-        { new: true }
-      );
-
-      // 2. If no counter exists yet, seed it based on your existing highest ticket
-      if (!counter) {
-        const result = await mongoose.model("TerminalFee").aggregate([
-          { $addFields: { numericTicketNo: { $toInt: "$ticketNo" } } },
-          { $sort: { numericTicketNo: -1 } },
-          { $limit: 1 }
-        ]);
-        
-        const maxTicket = result.length > 0 && result[0].numericTicketNo ? result[0].numericTicketNo : 0;
-        
-        counter = await Counter.create({ 
-          id: "ticketNo", 
-          seq: maxTicket + 1 
-        });
-      }
-
-      // 3. Assign the sequence to the ticket and proceed
-      this.ticketNo = counter.seq.toString();
-      next();
-    } catch (error) {
-      return next(error);
-    }
-  } else {
-    next();
-  }
-});
 
 export default mongoose.model("TerminalFee", terminalFeeSchema);
